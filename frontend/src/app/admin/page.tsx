806a4726bfb0e7b70c1dbd5ca1152daa23888f91
tsengel23 <tsengel0625@gmail.com>
2026-01-08 23:37:57 +0800

    ergelzee

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,30 +1,30 @@
-// "use client";
-
-// import { CategoryBar } from "./_components/CategoryBar";
-// import { FoodMenuSection } from "./_components/FoodMenuSection";
-
-// export default function AdminPage() {
-//   return (
-//     <div className="w-full h-screen border border-blue-500 flex flex-col gap-6 mt-6">
-//       <CategoryBar />
-//       <FoodMenuSection />
-//     </div>
-//   );
-// }
-
 "use client";
 
-import { useState } from "react";
-import { CategoryBar } from "../(public)/_components/CategoryBar";
-// import { CategoryBar } from "./_components/CategoryBar";
+import { CategoryBar } from "./_components/CategoryBar";
 import { FoodMenuSection } from "./_components/FoodMenuSection";
 
 export default function AdminPage() {
-  const [categoryId, setCategoryId] = useState<string>("");
   return (
     <div className="w-full h-screen border border-blue-500 flex flex-col gap-6 mt-6">
-      <CategoryBar categoryId={categoryId} />
+      <CategoryBar />
       <FoodMenuSection />
     </div>
   );
 }
+
+// "use client";
+
+// import { useState } from "react";
+// import { CategoryBar } from "../(public)/_components/CategoryBar";
+// // import { CategoryBar } from "./_components/CategoryBar";
+// import { FoodMenuSection } from "./_components/FoodMenuSection";
+
+// export default function AdminPage() {
+//   const [categoryId, setCategoryId] = useState<string>("");
+//   return (
+//     <div className="w-full h-screen border border-blue-500 flex flex-col gap-6 mt-6">
+//       <CategoryBar categoryId={categoryId} />
+//       <FoodMenuSection />
+//     </div>
+//   );
+// }
