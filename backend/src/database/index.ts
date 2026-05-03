13f8004408b176c1bc2a2831db5bff14b07b9737
tsengel23 <tsengel0625@email.com>
2026-05-03 10:32:20 +0800

    done myself

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -2,6 +2,6 @@ import { connect } from "mongoose";
 
 export const connectToDatabase = async () => {
   await connect(
-    "mongodb+srv://admin:iwEuo6f17Dq2hoal@cluster0.8k3n9aj.mongodb.net/?appName=Cluster0"
+    "mongodb://admin:iwEuo6f17Dq2hoal@ac-7yibc2u-shard-00-00.8k3n9aj.mongodb.net:27017,ac-7yibc2u-shard-00-01.8k3n9aj.mongodb.net:27017,ac-7yibc2u-shard-00-02.8k3n9aj.mongodb.net:27017/?authSource=admin&tls=true&appName=Cluster0"
   );
 };
